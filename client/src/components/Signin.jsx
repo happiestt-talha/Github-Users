ac5f830bcdaa47f75dc5e449e88edb75dedd136c
Talha <mtalhamanzoor786@gmail.com>
2024-07-05 16:29:51 +0500

    sign page

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,11 +1,25 @@
 //eslint-disable-next-line
 import React from 'react'
 
+import styles from '../css/Signin.module.css'
 const Signin = () => {
     return (
         <>
+            <div className={styles.container}>
+                <form>
+                    <h2>Sign In</h2>
+                    <div className={styles.form_group}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" />
+                    </div>
+                    <div className={styles.form_group}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" />
+                    </div>
+                    <button type="submit">Sign In</button>
+                </form>
+            </div >
 
-            <h1>Signin</h1>
         </>
     )
 }
