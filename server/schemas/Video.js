3953bffb584fde46cf826b82d8ade65e1ad23692
Talha <mtalhamanzoor786@gmail.com>
2024-07-11 20:08:10 +0500

    Added video schema

---
+++ server/schemas/Video.js
@@ -0,0 +1,20 @@
+import mongoose from mongoose
+
+const videoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
